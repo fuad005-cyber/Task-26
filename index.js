07e2e84f537d6b3f49070da0ebf6cd61256f0217
fuad005-cyber <fuadbores@gmail.com>
2024-02-06 16:19:59 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+// TASK 1
+// let number = +prompt("Iki reqemli ede daxil edin");
+// if (number < 10 || number > 99) {
+//   alert("Iki reqemli");
+// } else {
+//   while (number < 100) {
+//     number += 7;
+//     if (number >= 100) {
+//       break;
+//     }
+//   }
+//   alert("Sonuncu qiymet: " + number);
+// }
+
+// TASK 2
+// let ededler = +prompt("N ededini daxil edin:");
+// let count = 0;
+// while (count < ededler) {
+//   console.log("I know how to use cycles");
+//   count++;
+// }
+
+// TASK 3
+// let num = 90;
+// while (num < 1000 ) {
+//    num += 10;
+//    if (num == 1000) {
+//       break;
+//    }
+
+//    console.log(num);
+// }
+
+// TASK 4
+//  let sum = 0;
+//  let i = 10
+//  while (i <= 99) {
+//    if (i % 2 !== 0) {
+//      sum += i;
+//    }
+//    i++
+//  }
+//  console.log("Bütün ikirəqəmli tək ədədlərin cəmi: " + sum);
+
+// TASK 6 
+
+
+// TASK 7
+// let num = 1;
+// for (let i = 1; i < 10; i++) {
+//   num *= i
+// }
+// console.log(num);
+
+// TASK 8
+// let input = prompt("Bir metin daxil edin:");
+// let reverse = "";
+// for (let i = input.length - 1; i >= 0; i--) {
+//   reverse += input[i];
+// }
+// console.log(reverse);
+
+// TASK 9 -------------------------------------------
+
+// TASK 10
+// let input = prompt("Mətnd daxil edin");
+// let hasNumbers = false;
+// for (let i = 0; i < input.length; i++) {
+//   if (!isNaN(input[i])) {
+//     hasNumbers = true;
+//     break;
+//   }
+// }
+// if (hasNumbers) {
+//   console.log("Mətndə rəqəmlər var.");
+// } else {
+//   console.log("Mətndə rəqəmlər yoxdur.");
+// }
+
